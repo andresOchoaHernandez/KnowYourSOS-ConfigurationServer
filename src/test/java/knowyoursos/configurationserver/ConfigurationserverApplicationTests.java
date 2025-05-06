c433f07f065e791f454b076f4a666c1c64da2569
andresOchoaHernandez <ada.wong.gv@gmail.com>
2025-05-06 22:55:07 +0200

    Generated project with spring initializer

---
+++ src/test/java/knowyoursos/configurationserver/ConfigurationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package knowyoursos.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
