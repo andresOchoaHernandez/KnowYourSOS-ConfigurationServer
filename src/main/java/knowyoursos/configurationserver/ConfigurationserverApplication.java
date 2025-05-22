8c78027834d81b91cda8bd261e9ac59118d28be4
andresOchoaHernandez <ada.wong.gv@gmail.com>
2025-05-22 22:08:57 +0200

    Externalized configuration

--- src/main/java/knowyoursos/configurationserver/ConfigurationserverApplication.java
+++ src/main/java/knowyoursos/configurationserver/ConfigurationserverApplication.java
@@ -2,7 +2,9 @@ package knowyoursos.configurationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigurationserverApplication {
 
